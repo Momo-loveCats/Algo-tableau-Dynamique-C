de328232a628291a5f9a27ca37964e5552bea15b
momo <liamidi.sahyfdeen@gmail.com>
2025-09-18 22:57:13 +0200

    Ajout de ajoute element a un index

--- tableau.c
+++ tableau.c
@@ -12,7 +12,7 @@ int main()
     {
         printf("Donner une valeur et 0 pour quiiter : ");
         scanf("%i", &valeur);
-        ajouteFin(tab, valeur);
+        ajouteElementA(tab, 0, valeur);
         afficherTableau(tab);
         if (valeur == 0)
         {
@@ -115,4 +115,28 @@ int supprimerFin(tableauDynamique *tab)
     int valeur = get(tab, taille(tab) - 1);
     tab->taille -= 1;
     return valeur;
+}
+
+// Ajouter des element a un index compris dans les marges jusqua tab.taille incluse
+void ajouterElementA(tableauDynamique *tab, int index, int element)
+{
+    // le but c est d ajouter a l index et declarer le reste vers la droite
+    // la taille du tableau augmente
+    tab->taille += 1;
+    if (taille(tab) >= capacite(tab)) // si la taille est plus grande doubler le tableau
+    {
+        tab->capacite *= 2; // On double la capacite
+    }
+    int *temp = malloc(sizeof(int) * tab->capacite); // on cree un tableau temporaire
+    for (int i = 0; i < index; i++)                  // copie tous les elements avant index
+    {
+        temp[i] = get(tab, i);
+    }
+    temp[index] = element; // mettre index a sa valeur
+    for (int i = index; i < taille(tab) - 1; i++)
+    {
+        temp[i + 1] = get(tab, i);
+    }
+    free(tab->elements);
+    tab->elements = temp;
 }
